603cd97d1f0d17c880bec5f3b2afbf6831250958
Tehuan De Melo <tde-melo@lab1r5s2.42abudhabi.ae>
2023-02-20 17:21:44 +0400

    Philos were not dying due an problem with is_full function 		Due the parsing function nbr_of_meals is initialized 		with -1, in check_death is_philo_full always evaluated 		as true because -1 is a valid number 		To fix we just call is_philo_full if the number is 		bigger than zero

--- src/actions.c
+++ src/actions.c
@@ -6,7 +6,7 @@
 /*   By: tde-melo <tde-melo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 18:40:13 by tde-melo          #+#    #+#             */
-/*   Updated: 2023/02/20 15:35:39 by tde-melo         ###   ########.fr       */
+/*   Updated: 2023/02/20 17:19:33 by tde-melo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,10 +34,10 @@ void eating(philo_t *philo)
     print_status("\033[30;47m🍴 Has taken a fork \033[0m |\n", philo);
     print_status("\033[31;47m🍴 Has taken a fork \033[0m |\n", philo);
     print_status("\033[30;42m🍝 Is eating        \033[0m |\n", philo);
+    ft_usleep(get_time(), philo->dinner_info->time_to_eat * 1000);
     pthread_mutex_lock(&philo->dinner_info->is_dead_mtx);
     philo->last_meal_time = get_time();
     pthread_mutex_unlock(&philo->dinner_info->is_dead_mtx);
-    ft_usleep(get_time(), philo->dinner_info->time_to_eat * 1000);
     pthread_mutex_lock(&philo->left_mutex);
     philo->left_fork = 0;
     pthread_mutex_unlock(&philo->left_mutex);
--- src/check_death.c
+++ src/check_death.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   check_death.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tehuanmelo <tehuanmelo@student.42.fr>      +#+  +:+       +#+        */
+/*   By: tde-melo <tde-melo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 20:01:16 by tehuanmelo        #+#    #+#             */
-/*   Updated: 2023/02/19 18:18:27 by tehuanmelo       ###   ########.fr       */
+/*   Updated: 2023/02/20 17:17:52 by tde-melo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int is_philo_dead(philo_t *philo)
 {
     pthread_mutex_lock(&philo->dinner_info->is_dead_mtx);
     if ((get_time() - philo->last_meal_time) >= (philo->dinner_info->time_to_die * 1000))
-    {
+    { 
         print_status("\033[37;41m😵 is dead          \033[0m |\n", philo);
         pthread_mutex_lock(&philo->dinner_info->end_mtx);
         philo->dinner_info->end_dinner = 1;
@@ -31,7 +31,7 @@ int is_philo_dead(philo_t *philo)
 int if_philo_full_break(table_t *table, int philo)
 {
     pthread_mutex_lock(&table->is_full_mtx);
-    if (table->nbr_of_meals && (table->philosophers[philo].meals_count >= table->nbr_of_meals))
+    if (table->nbr_of_meals > 0  && (table->philosophers[philo].meals_count >= table->nbr_of_meals))
     {
         pthread_mutex_unlock(&table->is_full_mtx);
         return (1);
@@ -54,6 +54,10 @@ void check_death(table_t *table)
         int i = -1;
         while ((++i < table->nbr_of_philos))
         {
+            if (if_philo_full_break(table, i))
+                break;
+            if (is_philo_dead(&table->philosophers[i]))
+                break;
             pthread_mutex_lock(&table->end_mtx);
             if (table->end_dinner)
             {
@@ -61,10 +65,6 @@ void check_death(table_t *table)
                 break;
             }
             pthread_mutex_unlock(&table->end_mtx);
-            if (if_philo_full_break(table, i))
-                break;
-            if (is_philo_dead(&table->philosophers[i]))
-                break;
         }
     }
 }
